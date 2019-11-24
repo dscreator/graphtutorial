b05e230072920cad8c6d06c3411db8a32734d617
Devindra Chainani <devindra@msn.com>
2019-11-24 13:03:02 -0800

    updates for dependencies

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace graphtutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(".Net Core Tutorial");
+            int choice = -1;
+
+
+
+            while (choice != 0)
+            {
+                Console.WriteLine("0. To exit");
+                Console.WriteLine("1. Display access token");
+                Console.WriteLine("2. To exit");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        //display access token
+                        Console.WriteLine("The Access Token is: ");
+                        break;
+                    case 2:
+                        // display calendar events
+                        Console.WriteLine("The Calendar events are: ");
+                        break;
+
+
+                }
+
+            }
+            
+
         }
     }
 }
